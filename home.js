fbf8d1df589d5bf406a3597a366cb5c399d34f9a
Applepie46 <emmanuel.cadavid46@gmail.com>
2022-05-28 17:56:30 -0500

    Agragando html de login y de product detail

--- home.js
+++ home.js
@@ -17,3 +17,7 @@ app.get("/", (req, res) => {
 app.get("/registro", (req, res) => {
     res.sendFile(path.join(__dirname, "./views/registro.html"))
 });
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/login.html"))
+});
