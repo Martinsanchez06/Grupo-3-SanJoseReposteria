03527ba1a29fc56f4309c5b6120057b54c668d58
Martinsanchez06 <sanchezmartin2435gmail.com>
2022-05-29 16:25:20 -0500

    soluciono conflictos del home.js

--- home.js
+++ home.js
@@ -15,11 +15,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/registro", (req, res) => {
-<<<<<<< HEAD
     res.sendFile(path.join(__dirname, "./views/registro.html")) 
-=======
-    res.sendFile(path.join(__dirname, "./views/registro.html"))
->>>>>>> b167d3ade22b36372a1bd03c9bea7bface688811
 });
 
 app.get("/login", (req, res) => {
