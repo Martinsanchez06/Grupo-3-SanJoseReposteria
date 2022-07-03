a2c47650a77d993aa886f8da961cf626352cdd59
Applepie46 <emmanuel.cadavid46@gmail.com>
2022-07-03 09:27:45 -0500

    eliminando htmls

---
+++ home.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(3000, () => {
+    console.log("Proceso exitoso");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/home.html"))
+});
+
+app.get("/registro", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/registro.html")) 
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/login.html"))
+});
+
+app.get("/productdetail", (req, res) => {
+    res.sendFile(path.join(__dirname, "./views/productdetail.html"))
+});
