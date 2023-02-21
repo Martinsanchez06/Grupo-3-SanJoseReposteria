82194d61ff702e9e9df016f3cabf688dc954cbfb
Applepie46 <emmanuel.cadavid46@gmail.com>
2023-02-21 15:36:23 -0500

    [Emmanuel][v-01][fix] Adding changes to header en home silder

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,49 +1,49 @@
-module.exports = {
-  "development": {
-    "username": "freedb_userTest",
-    "password": "w&rgkFDC52*G7$Q",
-    "database": "freedb_san_jose_reposteria",
-    "host": "sql.freedb.tech",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "freedb_userTest",
-    "password": "w&rgkFDC52*G7$Q",
-    "database": "freedb_san_jose_reposteria",
-    "host": "sql.freedb.tech",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "freedb_userTest",
-    "password": "w&rgkFDC52*G7$Q",
-    "database": "freedb_san_jose_reposteria",
-    "host": "sql.freedb.tech",
-    "dialect": "mysql"
-  }
-}
 /*module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "san_jose_reposteria",
-    "host": "127.0.0.1",
+    "username": "freedb_mainUser",
+    "password": "@DTq9KQA*ux2w?9",
+    "database": "freedb_san_jose_reposteria",
+    "host": "sql.freedb.tech",
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "san_jose_reposteria",
-    "host": "127.0.0.1",
+    "username": "freedb_mainUser",
+    "password": "@DTq9KQA*ux2w?9",
+    "database": "freedb_san_jose_reposteria",
+    "host": "sql.freedb.tech",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "san_jose_reposteria",
-    "host": "127.0.0.1",
+    "username": "freedb_mainUser",
+    "password": "@DTq9KQA*ux2w?9",
+    "database": "freedb_san_jose_reposteria",
+    "host": "sql.freedb.tech",
     "dialect": "mysql"
   }
 }*/
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": null,
+    "database": "san_jose_reposteria",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "san_jose_reposteria",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "san_jose_reposteria",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
 /*module.exports= {
   "development": {
     "username": "GNDB5tewrN",
