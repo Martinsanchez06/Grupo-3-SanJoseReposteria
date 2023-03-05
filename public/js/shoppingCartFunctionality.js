318fa63a85629c57ea2796d3ab5ec5f4d3f823c5
Applepie46 <emmanuel.cadavid46@gmail.com>
2023-03-05 13:27:14 -0500

    [Emmanuel][feat][v-0.2] final details

--- public/js/shoppingCartFunctionality.js
+++ public/js/shoppingCartFunctionality.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", (e) => {
     let products = [];
-    let cartRow = document.querySelector(".cart-row");
+    const cartRow = document.querySelector(".cart-row");
+    const deleteFromCartBtn = document.querySelectorAll("#delete-btn");
+    console.log(deleteFromCartBtn);
+
 
     function emptyShopingCart() {
 
@@ -26,9 +29,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
             fetch(`/api/productDetail/${item.id}`)
                 .then(resp => resp.json())
                 .then(product => {
-                    console.log(product.imagesUrl.image_1);
                     if (product) {
-                        console.log(item.quantity);
                         products.push({
                             image: product.imagesUrl.image_1,
                             originalProductId: product.data.id,
@@ -39,27 +40,27 @@ document.addEventListener("DOMContentLoaded", (e) => {
                         cartRow.innerHTML +=
                             `
                             <div class="d-flex justify-content-between mb-2 p-2 align-items-center">
-                            <div class="w-100 d-flex align-items-center ">
-                            <div class="cart-image-container me-2">
-                                <a class="w-100" href="/productos/productdetail/${product.data.id}">
-                                    <img class="w-100" src="${product.imagesUrl.image_1}" alt="">
-                                </a>
+                                <div class="w-100 d-flex align-items-center ">
+                                    <div class="cart-image-container me-2">
+                                        <a class="w-100" href="/productos/productdetail/${product.data.id}">
+                                            <img class="w-100" src="${product.imagesUrl.image_1}" alt="">
+                                        </a>
+                                    </div>
+                                    <div class="w-75 cart-name-container">
+                                        <p class="mb-0"><strong>${product.data.name}</strong></p>
+                                    </div>
+                                </div>
+                                <div class="w-100 d-flex justify-content-center align-items-center quantity-container">
+                                    <span>Cantidad</span>
+                                    <span class="d-flex quantity-input"><input class="rounded-pill" type="number" min="0" max="10" value="${product.quantity}"></span>
+                                </div>
+                                <div class="">
+                                    <p class="mb-0"><strong>$${product.data.price}</strong></p>
+                                </div>
+                                <div class="p-3 delete-btn-cart">
+                                    <button class="delete-from-cart" id="delete-btn" onClick="window.location.reload();"><i class="text-danger fa-solid fa-trash delete-from-cart"></i></button>
+                                </div>
                             </div>
-                            <div class="w-75 cart-name-container">
-                                <p class="mb-0"><strong>${product.data.name}</strong></p>
-                            </div>
-                        </div>
-                        <div class="w-100 d-flex justify-content-center align-items-center quantity-container">
-                            <span>Cantidad</span>
-                        <span class="d-flex quantity-input"><input class="rounded-pill" type="number" min="0" max="10" value="${product.quantity}"></span>
-                        </div>
-                        <div class="">
-                            <p class="mb-0"><strong>$${product.data.price}</strong></p>
-                        </div>
-                        <div class="p-3 delete-btn-cart">
-                        <button><i class="text-danger fa-solid fa-trash delete-from-cart"></i></button>
-                        </div>
-                        </div>
                         `
                     } else {
                         shoppingCart.splice(index, 1);
